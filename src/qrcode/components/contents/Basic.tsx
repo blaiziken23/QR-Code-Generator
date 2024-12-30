12163af73365b64605f58bd37fde5a5308c1ec82
Jayvie <jayviee.berdin@gmail.com>
2024-12-30 16:50:08 +0800

    almost done

--- src/qrcode/components/contents/Basic.tsx
+++ src/qrcode/components/contents/Basic.tsx
@@ -9,32 +9,58 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { QRProps } from "@/qrcode/types";
+import { level } from "@/qrcode/utils/constants";
+import { useQRCodeStore } from "@/qrcode/zustand";
+import { ChangeEvent } from "react";
 
 const Basic = () => {
+  const { onChange } = useQRCodeStore();
+  const { size, value } = useQRCodeStore().data;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    onChange({ [id]: value });
+  };
+
   return (
     <div className="space-y-6">
-      <div className="grid w-full items-center gap-3">
-        <Label htmlFor="text">Input URL or Texts</Label>
-        <Input type="text" id="text" placeholder="URL / Texts" />
+      <div className="flex flex-col gap-3">
+        <Label htmlFor="value">Input URL or Texts</Label>
+        <Input
+          type="text"
+          id="value"
+          value={value}
+          placeholder="URL / Texts"
+          onChange={handleChange}
+        />
       </div>
-      <div className="grid w-full items-center gap-3">
+      <div className="flex flex-col gap-3">
         <Label htmlFor="size">Size</Label>
-        <Input type="number" id="size" placeholder="Size" />
+        <Input
+          type="number"
+          id="size"
+          placeholder="Size"
+          value={size}
+          onChange={handleChange}
+        />
       </div>
-      <div className="grid w-full items-center gap-3">
+      <div className="flex flex-col gap-3">
         <Label htmlFor="error">Error Correction Level</Label>
-        <Select>
-          <SelectTrigger id="error" className="">
-            <SelectValue placeholder="Select a fruit" />
+        <Select
+          onValueChange={(e) => onChange({ level: e as QRProps["level"] })}
+        >
+          <SelectTrigger className="">
+            <SelectValue placeholder="Select a level" />
           </SelectTrigger>
-          <SelectContent>
+          <SelectContent id="level" onChange={(e) => console.log(e)}>
             <SelectGroup>
-              <SelectLabel>Fruits</SelectLabel>
-              <SelectItem value="apple">Apple</SelectItem>
-              <SelectItem value="banana">Banana</SelectItem>
-              <SelectItem value="blueberry">Blueberry</SelectItem>
-              <SelectItem value="grapes">Grapes</SelectItem>
-              <SelectItem value="pineapple">Pineapple</SelectItem>
+              <SelectLabel>Error Correction level</SelectLabel>
+              {level.map((data, i) => (
+                <SelectItem value={data.value} key={i}>
+                  {data.desc}
+                </SelectItem>
+              ))}
             </SelectGroup>
           </SelectContent>
         </Select>
