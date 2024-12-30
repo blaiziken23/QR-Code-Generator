691ebc9fc1c03da2df5547d28490b449c72a2b11
Jayvie <jayviee.berdin@gmail.com>
2024-12-30 11:01:53 +0800

    set up

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import GradientBg from "./qrcode/components/GradientBg";
+import Headline from "./qrcode/components/Headline";
+import Generator from "./qrcode/components/Generator";
+import SvgEffects from "./qrcode/components/SvgEffects";  
+
+function App() {
+  return (
+    <div className="relative h-screen">
+      <SvgEffects />
+      <GradientBg />
+      <div className="relative z-10">
+        <div className="container mx-auto p-6 flex justify-center">
+          <div className="max-w-6xl w-full">
+            <Headline />
+            <Generator />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
