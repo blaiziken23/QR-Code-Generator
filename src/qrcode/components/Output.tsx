ddfdb76dcc4c880048721b955497349923f2822d
Jayvie <jayviee.berdin@gmail.com>
2024-12-30 16:49:57 +0800

    display output

--- src/qrcode/components/Output.tsx
+++ src/qrcode/components/Output.tsx
@@ -1,5 +1,12 @@
+import { QRCodeSVG } from "qrcode.react";
+import { useQRCodeStore } from "../zustand";
+
 const Output = () => {
-  return <div>Output</div>;
+  return (
+    <div className="flex justify-center items-center">
+      <QRCodeSVG {...useQRCodeStore().data} />
+    </div>
+  );
 };
 
 export default Output;
