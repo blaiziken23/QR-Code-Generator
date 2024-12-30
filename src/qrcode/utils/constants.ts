5baa556dfce1e9178d429988f576ccf381cee8c1
Jayvie <jayviee.berdin@gmail.com>
2024-12-30 16:49:46 +0800

    create const

---
+++ src/qrcode/utils/constants.ts
@@ -0,0 +1,18 @@
+export const level = [
+  {
+    value: "L",
+    desc: "Low ",
+  },
+  {
+    value: "M",
+    desc: "Medium",
+  },
+  {
+    value: "Q",
+    desc: "Quartile",
+  },
+  {
+    value: "H",
+    desc: "High",
+  },
+] as const;
