1fcde682a559e723217ab39277ded1131827e402
Jayvie <jayviee.berdin@gmail.com>
2024-12-31 13:12:07 +0800

    create download

--- src/qrcode/components/Output.tsx
+++ src/qrcode/components/Output.tsx
@@ -2,14 +2,41 @@ import { QRCodeSVG } from "qrcode.react";
 import { useQRCodeStore } from "../zustand";
 import { Button } from "@/components/ui/button";
 import { DownloadIcon } from "lucide-react";
+import { useRef } from "react";
 
 const Output = () => {
+  const qrCodeRef = useRef<SVGSVGElement>(null);
+  const { QRImageSettings, QRdata } = useQRCodeStore();
+
+  const downloadQRCode = () => {
+    if (qrCodeRef.current) {
+      const svgData = new XMLSerializer().serializeToString(qrCodeRef.current);
+      const svgBlob = new Blob([svgData], {
+        type: "image/svg+xml;charset=utf-8",
+      });
+      const svgUrl = URL.createObjectURL(svgBlob);
+      const downloadLink = document.createElement("a");
+      downloadLink.href = svgUrl;
+      downloadLink.download = "qrcode.svg";
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+      URL.revokeObjectURL(svgUrl);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center flex-col space-y-4">
-      <QRCodeSVG {...useQRCodeStore().data} />
+      <div className="relative max-w-80">
+        <QRCodeSVG
+          {...QRdata}
+          imageSettings={QRImageSettings}
+          ref={qrCodeRef}
+        />
+      </div>
       <Button
         className="w-full max-w-sm rounded-full bg-[#006D77] hover:bg-[#006D77]/80"
-        // onClick={onClick}
+        onClick={downloadQRCode}
       >
         <DownloadIcon /> Download
       </Button>
